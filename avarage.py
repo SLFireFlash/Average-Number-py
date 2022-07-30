59f5968c11677cfb70ba9180a4513cb74e4198a5
sl fireflash <80664734+SLFireFlash@users.noreply.github.com>
2022-07-30 20:24:08 +0530

    Add files via upload

---
+++ avarage.py
@@ -0,0 +1,11 @@
+print("****Average number***")
+number_list =[]
+x=0
+top=0
+rep=int(input("how many numbers:-"))
+for num in range(0,rep):
+	numberL=float(input("enter number"))
+	top +=numberL
+AverageL=top/rep
+print(AverageL)
+input()
